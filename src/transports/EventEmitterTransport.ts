d677e0bf99d1500545a34b32f9f087e70bce5242
shanejonas <jonas.shane@gmail.com>
2019-05-14 10:15:59 -0700

    fix: remove null checks on Event Emitter

--- src/transports/EventEmitterTransport.ts
+++ src/transports/EventEmitterTransport.ts
@@ -2,34 +2,23 @@ import { EventEmitter } from "events";
 import ITransport from "./Transport";
 
 class EventEmitterTransport implements ITransport {
-  public connection: EventEmitter | null;
+  public connection: EventEmitter;
   constructor(uri: string) {
     this.connection = new EventEmitter();
   }
   public connect(): Promise<any> {
-    // noop
     return Promise.resolve();
   }
   public onData(callback: any) {
-    if (!this.connection) {
-      return;
-    }
     this.connection.addListener("message", (data: any) => {
       callback(data);
     });
   }
   public sendData(data: any) {
-    if (!this.connection) {
-      return;
-    }
     this.connection.emit("message", data);
   }
   public close() {
-    if (!this.connection) {
-      return;
-    }
     this.connection.removeAllListeners();
-    this.connection = null;
   }
 }
 
