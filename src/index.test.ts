8bcf352b9770e3126a02680954d2337d84dc37e5
Zachary Belford <belfordz66@gmail.com>
2019-07-26 12:47:29 -0700

    fix: improve coverage on index

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,9 @@ import Client from ".";
 import RequestManager from "./RequestManager";
 import EventEmitterTransport from "./transports/EventEmitterTransport";
 
+jest.mock("./RequestManager");
+
+const mockedRequestManager = RequestManager as jest.Mock<RequestManager>;
 describe("client-js", () => {
   it("can be constructed", () => {
     const c = new Client(new RequestManager([new EventEmitterTransport("foo://unique")]));
@@ -14,4 +17,20 @@ describe("client-js", () => {
     expect(typeof c.request("my_method", null).then).toEqual("function");
   });
 
+  describe("startBatch", () => {
+    it("calls the requestManager.startBatch", () => {
+      const rm = new mockedRequestManager([new EventEmitterTransport("foo://unique")]);
+      const c = new Client(rm);
+      c.startBatch();
+      expect(mockedRequestManager.mock.instances[0].startBatch).toHaveBeenCalled();
+    });
+  });
+
+  describe("stopBatch", () => {
+    const rm = new RequestManager([new EventEmitterTransport("foo://unique")]);
+    const c = new Client(rm);
+    c.startBatch();
+    c.stopBatch();
+    expect(mockedRequestManager.mock.instances[0].startBatch).toHaveBeenCalled();
+  });
 });
