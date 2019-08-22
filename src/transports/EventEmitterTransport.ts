b125962cb311f8d9a9ef806b8a453130d2d2bed7
shanejonas <jonas.shane@gmail.com>
2019-08-22 08:50:16 -0700

    fix(transports): handle multiple onData callback
    fixes #53

--- src/transports/EventEmitterTransport.ts
+++ src/transports/EventEmitterTransport.ts
@@ -5,20 +5,25 @@ class EventEmitterTransport implements ITransport {
   public connection: EventEmitter;
   private reqUri: string;
   private resUri: string;
+  private onDataCallbacks: any[];
   constructor(emitter: EventEmitter, reqUri: string, resUri: string) {
+    this.onDataCallbacks = [];
     this.connection = emitter;
     this.reqUri = reqUri;
     this.resUri = resUri;
   }
 
   public connect(): Promise<any> {
+    this.connection.on(this.reqUri, (data: any) => {
+      this.onDataCallbacks.map((callback: (data: string) => void) => {
+        callback(data);
+      });
+    });
     return Promise.resolve();
   }
 
-  public onData(callback: (data: string) => any) {
-    this.connection.on(this.reqUri, (data: any) => {
-      callback(data);
-    });
+  public onData(callback: (data: string) => void) {
+    this.onDataCallbacks.push(callback);
   }
 
   public sendData(data: string) {
