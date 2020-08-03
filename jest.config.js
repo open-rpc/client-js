6f26276ff39b1544a378a8928b83c1a5531759d9
Agost Biro <agostbiro@gmail.com>
2020-08-03 11:59:09 +0200

    feat: add options to HTTPTransport constructor
    - add credentials and headers options to HTTPTransport that are passed
    on to fetch
    - disable resetMocks in jest.config.js as it resets reusable spies to
    no-op implementation

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   "clearMocks": true,
   "coverageDirectory": "../coverage",
-  "resetMocks": true,
+  // `resetMocks` resets reusable spies from __mocks__ to no-op implementation
+  // see: https://stackoverflow.com/a/63191062/2650622 for more details
+  "resetMocks": false,
   "restoreMocks": true,
   "rootDir": "./src",
   "testEnvironment": "jsdom",
